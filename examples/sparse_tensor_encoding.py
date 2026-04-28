77f9d5c54816fc4fa927c9eb57a21472f21c877c
Vito Secona <secona00@gmail.com>
2026-04-28 15:14:53 +0700

    implement sparse matrix addition

--- examples/sparse_tensor_encoding.py
+++ examples/sparse_tensor_encoding.py
@@ -1,9 +1,37 @@
+import jax
 from spjax import SparseTensor
 
 
+def print_sparse_matrix(m: SparseTensor):
+    rows = m.row.tolist()
+    cols = m.col.tolist()
+    vals = m.values.tolist()
+
+    num_rows = max(m.row) + 1
+    num_cols = max(m.col) + 1
+
+    sparse_map = {}
+    for r, c, v in zip(rows, cols, vals):
+        coords = (int(r), int(c))
+        sparse_map[coords] = sparse_map.get(coords, 0) + v
+
+    for r in range(num_rows):
+        row_str = []
+        for c in range(num_cols):
+            val = int(sparse_map.get((r, c), 0))
+            row_str.append(f"{val:>4}" if val != 0 else "   .")
+        print(" ".join(row_str))
+
+@jax.jit
+def add(x, y):
+    return x + y
+
+
 def main() -> None:
-    m = SparseTensor.from_file("./matrix/ibm32.mtx")
-    print("nnz:", m.nnz)
+    a = SparseTensor.from_file("./matrix/ibm32.mtx")
+    b = SparseTensor.from_file("./matrix/Hamrle1.mtx")
+    c = add(a, b)
+    print_sparse_matrix(c)
 
 
 if __name__ == "__main__":
--- src/spjax/tensor.py
+++ src/spjax/tensor.py
@@ -1,10 +1,25 @@
+import jax.numpy as jnp
 from jax.tree_util import register_pytree_node
 
 class SparseTensor:
     nnz: int
 
-    def __init__(self, nnz) -> None:
+    def __init__(self, nnz, values, row, col) -> None:
         self.nnz = nnz
+        self.values = jnp.asarray(values)
+        self.row = jnp.asarray(row)
+        self.col = jnp.asarray(col)
+
+    def __add__(self, other) -> SparseTensor:
+        if isinstance(other, SparseTensor):
+            new_values = jnp.concatenate([self.values, other.values])
+            new_row = jnp.concatenate([self.row, other.row])
+            new_col = jnp.concatenate([self.col, other.col])
+            nnz = len(new_values)
+
+            return SparseTensor(nnz, new_values, new_row, new_col)
+
+        raise NotImplemented
 
     @classmethod
     def from_file(cls, filename) -> SparseTensor:
@@ -14,14 +29,16 @@ class SparseTensor:
             raise ImportError("Failed to import SciPy for reading sparse tensor")
 
         m_coo = scipy.io.mmread(filename)
-        return SparseTensor(m_coo.nnz)
+        return SparseTensor(m_coo.nnz, m_coo.data, m_coo.row, m_coo.col)
 
 def sparse_tensor_flatten(obj):
-    children = (obj.nnz,)
-    aux_data = None
+    children = (obj.values, obj.row, obj.col)
+    aux_data = (obj.nnz,)
     return (children, aux_data)
 
-def sparse_tensor_unflatten(_, children):
-    return SparseTensor(*children)
+def sparse_tensor_unflatten(aux_data, children):
+    values, row, col = children
+    nnz, = aux_data
+    return SparseTensor(nnz, values, row, col)
 
 register_pytree_node(SparseTensor, sparse_tensor_flatten, sparse_tensor_unflatten)
