35a6a4502a19d076bad346d1c0d1108ac2a2ffa5
Vito Secona <secona00@gmail.com>
2026-04-28 09:59:34 +0700

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python313;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            python
+            pkgs.uv
+          ];
+        };
+      }
+    );
+}
