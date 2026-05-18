3ee806c36f9e758c318e632d201cdb5d3d7daf29
Vito Secona <secona00@gmail.com>
2026-05-18 19:14:57 +0700

    add LevelType

--- src/spjax/encoding.py
+++ src/spjax/encoding.py
@@ -1,5 +1,45 @@
-from dataclasses import dataclass
+import jax
 from enum import Enum
+from abc import ABC, abstractmethod
+
+
+class LevelType(ABC):
+    @abstractmethod
+    def pos_bounds(self, p_k_minus_1) -> tuple[int, int]: ...
+
+    @abstractmethod
+    def pos_access(self, p_k) -> tuple[int, bool]: ...
+
+
+class CompressedLevel(LevelType):
+    pos: jax.Array
+    crd: jax.Array
+
+    def __init__(self, pos, crd) -> None:
+        self.pos = pos
+        self.crd = crd
+
+    def pos_bounds(self, p_k_minus_1) -> tuple[int, int]:
+        p_begin = int(self.pos[p_k_minus_1])
+        p_end = int(self.pos[p_k_minus_1 + 1])
+        return p_begin, p_end
+
+    def pos_access(self, p_k) -> tuple[int, bool]:
+        i_k = int(self.crd[p_k])
+        return i_k, True
+
+class SingletonLevel(LevelType):
+    crd: jax.Array
+
+    def __init__(self, crd) -> None:
+        self.crd = crd
+
+    def pos_bounds(self, p_k_minus_1) -> tuple[int, int]:
+        return p_k_minus_1, p_k_minus_1 + 1
+
+    def pos_access(self, p_k) -> tuple[int, bool]:
+        i_k = int(self.crd[p_k])
+        return i_k, True
 
 
 class LevelFormat(str, Enum):
