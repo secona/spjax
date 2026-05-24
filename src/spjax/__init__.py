dfd9bfdadc1f8b860083013fabe6c7612762cdf7
Vito Secona <secona00@gmail.com>
2026-05-24 15:35:16 +0700

    remove old

--- src/spjax/__init__.py
+++ src/spjax/__init__.py
@@ -1,10 +1,7 @@
-from .encoding import LevelFormat, SparseEncoding
 from .primitive import sparse_dot
 from .tensor import SparseTensor
 
 __all__ = [
-    "LevelFormat",
-    "SparseEncoding",
     "SparseTensor",
     "sparse_dot",
 ]
--- src/spjax/encoding.py
+++ src/spjax/encoding.py
@@ -109,26 +109,3 @@ class SingletonLevel(LevelType):
         crd, = children
         return cls(crd)
 
-
-class LevelFormat(str, Enum):
-    DENSE = "dense"
-    COMPRESSED = "compressed"
-    SINGLETON = "singleton"
-    BLOCKED = "blocked"
-
-
-class Dimension:
-    name: str
-
-    def __init__(self, name):
-        self.name = name
-
-
-class SparseEncoding:
-    lvl_format: tuple[LevelFormat, ...]
-    dims: list[Dimension]
-    lvls: dict[Dimension, LevelFormat]
-
-    def __init__(self, dims: list[Dimension], lvls: dict[Dimension, LevelFormat]):
-        self.dims = dims
-        self.lvls = lvls
