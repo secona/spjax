2612149e0e0af3195a4d380d7fd6d17be04b85ce
Vito Secona <secona00@gmail.com>
2026-05-27 06:10:39 +0700

    spdot: first implementation

--- examples/spdot.py
+++ examples/spdot.py
@@ -3,11 +3,19 @@ import jax.numpy as jnp
 
 from spjax import encoding
 from spjax.tensor import SparseTensor
-from spjax.primitive import sparse_dot
 
-@jax.jit
-def spdot(a, b):
-    return sparse_dot(a, b)
+@jax.jit(static_argnums=(2,))
+def spdot(a, b, size):
+    _, idx_a, idx_b = jnp.intersect1d(
+        a.lvls[0].crd,
+        b.lvls[0].crd,
+        size=size,
+        fill_value=0,
+        return_indices=True
+    )
+
+    products = a.values[idx_a] * b.values[idx_b]
+    return jnp.sum(products)
 
 def main() -> None:
     vals = jnp.array([1, 2, 3, 4])
@@ -19,8 +27,9 @@ def main() -> None:
 
     a = SparseTensor(vals, shape, [lvl])
     b = SparseTensor(vals, shape, [lvl])
-    print(spdot(a, b))
-    print(spdot.lower(a, b).as_text())
+    size = a.lvls[0].pos[1].item()
+    print(spdot(a, b, size))
+    print(spdot.lower(a, b, size).as_text())
 
 if __name__ == '__main__':
     main()
