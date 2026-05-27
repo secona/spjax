e794b34677db75a2e68bfcde2e1e6adbecc366fc
Vito Secona <secona00@gmail.com>
2026-05-27 08:33:25 +0700

    spdot: add densify method

--- examples/spdot.py
+++ examples/spdot.py
@@ -17,6 +17,15 @@ def spdot(a, b, size):
     products = a.values[idx_a] * b.values[idx_b]
     return jnp.sum(products)
 
+@jax.jit(static_argnums=(2,))
+def spdot_densify(a, b, size):
+    a_crd = a.lvls[0].crd
+    b_crd = b.lvls[0].crd
+    dense_a = jnp.zeros(size, dtype=a.values.dtype)
+    dense_a = dense_a.at[a_crd].set(a.values)
+    products = dense_a[b_crd] * b.values
+    return jnp.sum(products)
+
 def main() -> None:
     vals = jnp.array([1, 2, 3, 4])
     shape = (10,)
@@ -27,9 +36,14 @@ def main() -> None:
 
     a = SparseTensor(vals, shape, [lvl])
     b = SparseTensor(vals, shape, [lvl])
+
     size = a.lvls[0].pos[1].item()
     print(spdot(a, b, size))
     print(spdot.lower(a, b, size).as_text())
+
+    size = a.shape[0]
+    print(spdot_densify(a, b, size))
+    print(spdot_densify.lower(a, b, size).as_text())
 
 if __name__ == '__main__':
     main()
